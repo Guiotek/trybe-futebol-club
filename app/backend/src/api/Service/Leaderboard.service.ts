f85296e146cb8d178443d8c6cad0ce8e32dac192
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2023-03-09 17:03:28 -0300

    feat: req 24

--- app/backend/src/api/Service/Leaderboard.service.ts
+++ app/backend/src/api/Service/Leaderboard.service.ts
@@ -1,26 +1,33 @@
 import Model from '../../database/models';
 
 const query = `select team_name as name,
-count(mt.home_team_id) as totalGames,
 sum(case
 when home_team_goals > away_team_goals then 3
 when home_team_goals = away_team_goals then 1
 else 0
 end ) as totalPoints,
-sum(home_team_goals) as goalsFavor,
-sum(away_team_goals) as goalsOwn,
+count(home_team_id) as totalGames,
 sum(case
 when home_team_goals > away_team_goals then 1
 else 0
 end) as totalVictories,
 sum(case
+when home_team_goals = away_team_goals then 1
+else 0
+end) as totalDraws,
+sum(case
 when home_team_goals < away_team_goals then 1
 else 0
 end) as totalLosses,
-sum(case
+sum(home_team_goals) as goalsFavor,
+sum(away_team_goals) as goalsOwn,
+sum(home_team_goals - away_team_goals) as goalsBalance,
+floor(sum(case
+when home_team_goals > away_team_goals then 3
 when home_team_goals = away_team_goals then 1
 else 0
-end) as totalDraws
+end) / 
+(count(home_team_id) * 3) * 100) as efficiency
 from teams as t
 inner join matches as mt
 on mt.home_team_id = t.id
