8c6502d1f823dcae9227ac5053c7a087d33fe84b
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2023-03-09 18:15:35 -0300

    fix: req 24

--- app/backend/src/api/Service/Leaderboard.service.ts
+++ app/backend/src/api/Service/Leaderboard.service.ts
@@ -22,17 +22,22 @@ end) as totalLosses,
 sum(home_team_goals) as goalsFavor,
 sum(away_team_goals) as goalsOwn,
 sum(home_team_goals - away_team_goals) as goalsBalance,
-floor(sum(case
+round(sum(case
 when home_team_goals > away_team_goals then 3
 when home_team_goals = away_team_goals then 1
 else 0
 end) / 
-(count(home_team_id) * 3) * 100) as efficiency
+(count(home_team_id) * 3) * 100, 2) as efficiency
 from teams as t
 inner join matches as mt
 on mt.home_team_id = t.id
 and mt.in_progress = 0
-group by t.id`;
+group by t.id
+order by totalVictories desc,
+goalsBalance desc,
+goalsFavor desc,
+goalsOwn desc
+`;
 
 export default class LeaderboardService {
   public getAll = async () => {
